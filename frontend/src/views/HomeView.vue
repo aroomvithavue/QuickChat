f87ed19278f150e59cb50b9cc8adaf067e221202
Lavrans Kaul Sagen <lavrans.ks@gmail.com>
2022-11-21 16:10:03 -0500

    Add Frontend Scaffolding
    This fixes #2

---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="home">
+    <div class="card">
+      <figure>
+        <img alt="Vue logo" src="../assets/logo.png" />
+      </figure>
+      <div class="card-body">
+        <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import HelloWorld from "@/components/HelloWorld.vue"; // @ is an alias to /src
+
+export default Vue.extend({
+  name: "HomeView",
+  components: {
+    HelloWorld,
+  },
+});
+</script>
