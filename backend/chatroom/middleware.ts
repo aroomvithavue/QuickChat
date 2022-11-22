11869554865e302f8f5fbd45ca6aa13bf5c0e83a
Helena <hmerker@mit.edu>
2022-11-22 18:40:30 -0500

    chatroom router, middleware, and util

---
+++ backend/chatroom/middleware.ts
@@ -0,0 +1,103 @@
+import type {Request, Response, NextFunction} from 'express';
+import {Types} from 'mongoose';
+import ChatRoomCollection from './collection';
+
+/**
+ * Checks if a chat room with keyword exists
+ */
+const doesChatRoomWithKeyExist = async (req: Request, res: Response, next: NextFunction) => {
+  const chatRoom = await ChatRoomCollection.findByKeyword(req.params.keyword as string);
+  if (!chatRoom) {
+    res.status(404).json({
+      error: `Chat room with keyword of ${req.params.keyword} does not exist.`
+    });
+    return;
+  }
+
+  next();
+};
+
+/**
+ * Checks if a chat room with chatRoomId exists
+ */
+ const doesChatRoomExist = async (req: Request, res: Response, next: NextFunction) => {
+    const validFormat = Types.ObjectId.isValid(req.params.chatRoomId);
+    const chatRoom = validFormat ? await ChatRoomCollection.findOne(req.params.chatRoomId) : '';
+    if (!chatRoom) {
+      res.status(404).json({
+        error: `Chat room with chat room ID ${req.params.chatRoomId} does not exist.`
+      });
+      return;
+    }
+  
+    next();
+  };
+
+/**
+ * Checks if the new chat room is valid.
+ */
+const isValidChatRoom = async (req: Request, res: Response, next: NextFunction) => {
+  const keyword = req.body.keyword as string;
+  if (!keyword.trim()) {
+    res.status(400).json({
+      error: 'Keyword must be at least one character long.'
+    });
+    return;
+  }
+  
+  const chatRoom = await ChatRoomCollection.findByKeyword(keyword);
+  if (chatRoom) {
+    res.status(400).json({
+      error: `Chat room with keyword of ${req.params.keyword} already exists.`
+    });
+    return;
+  }
+
+  const days = req.body.days as string;
+  if (Number(days) === NaN || Number(days) <= 0 || Number(days) !== parseInt(days)) {
+    res.status(400).json({
+      error: 'Days must be a positive integer.'
+    });
+    return;
+  }
+
+  const hours = req.body.hours as string;
+  if (Number(hours) === NaN || Number(hours) <= 0 || Number(hours) !== parseInt(hours)) {
+    res.status(400).json({
+      error: 'Hours must be a positive integer.'
+    });
+    return;
+  }
+
+  next();
+};
+
+/**
+ * Checks if the new message is valid.
+ */
+ const isValidMessage = async (req: Request, res: Response, next: NextFunction) => {
+    const message = req.body.message as string;
+    if (!message.trim()) {
+        res.status(400).json({
+        error: 'Message must be at least one character long.'
+        });
+        return;
+    }
+
+    const author = req.body.author as string;
+    if (!author.trim()) {
+        res.status(400).json({
+        error: 'Author must be at least one character long.'
+        });
+        return;
+    }
+  
+    next();
+  };
+
+export {
+  doesChatRoomWithKeyExist,
+  doesChatRoomExist,
+  isValidChatRoom,
+  isValidMessage
+};
---
+++ backend/chatroom/util.ts
@@ -0,0 +1,46 @@
+import type {HydratedDocument} from 'mongoose';
+import moment from 'moment';
+import type {ChatRoom, PopulatedChatRoom} from './model';
+
+type ChatRoomResponse = {
+  _id: string;
+  keyword: string;
+  dateCreated: string;
+  dateExpired: string;
+  messages: Array<{text: String, date: Date, author: String}>;
+};
+
+/**
+ * Encode a date as an unambiguous string
+ *
+ * @param {Date} date - A date object
+ * @returns {string} - formatted date as string
+ */
+const formatDate = (date: Date): string => moment(date).format('MMMM Do YYYY, h:mm:ss a');
+
+/**
+ * Transform a raw ChatRoom object from the database into an object
+ * with all the information needed by the frontend
+ *
+ * @param {HydratedDocument<ChatRoom>} chatRoom - a chat room
+ * @returns {ChatRoomResponse} - the chat room object formatted for the frontend
+ */
+const constructChatRoomResponse = (chatRoom: HydratedDocument<ChatRoom>): ChatRoomResponse => {
+  const chatRoomCopy: PopulatedChatRoom = {
+    ...chatRoom.toObject({
+      versionKey: false // Cosmetics; prevents returning of __v property
+    })
+  };
+  return {
+    ...chatRoomCopy,
+    _id: chatRoomCopy._id.toString(),
+    keyword: chatRoom.keyword,
+    dateCreated: formatDate(chatRoom.dateCreated),
+    dateExpired: formatDate(chatRoom.dateExpired),
+    messages: chatRoom.messages
+  };
+};
+
+export {
+  constructChatRoomResponse
+};
