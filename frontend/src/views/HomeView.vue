b26ca85c8c35e01a67af3e1f53eccdf56dccdef1
Ashar Farooq <afarooq@mit.edu>
2022-11-29 20:58:00 -0500

    fixing routes and page for basic home

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,8 +1,37 @@
 <template>
-  <div class="about">
+  <main>
     <h1>
       This is home view for the path / (likely the chat joining or generating
       page stuff). Go to /chat for the chat view and /about for the about view
     </h1>
-  </div>
+
+    <center>
+      <div class="card w-96 bg-base-100 shadow-xl">
+        <div class="card-body">
+          <h2 class="card-title">Create Chat</h2>
+          <p>Duration</p>
+          <div class="card-actions justify-end">
+            <a href="#/chat" class="link link-primary"
+              ><button class="btn btn-primary">Generate Chat</button></a
+            >
+          </div>
+        </div>
+      </div>
+
+      <div class="card w-96 bg-base-100 shadow-xl">
+        <div class="card-body">
+          <h2 class="card-title">Join Chat</h2>
+          <p>Keyword</p>
+          <div class="card-actions justify-end">
+            <a href="#/chat" class="link link-primary"
+              ><button class="btn btn-primary">Join Chat</button></a
+            >
+          </div>
+        </div>
+      </div>
+      <a href="#/about" class="link link-primary">
+        <button class="btn btn-primary">About QuickChat</button></a
+      >
+    </center>
+  </main>
 </template>
